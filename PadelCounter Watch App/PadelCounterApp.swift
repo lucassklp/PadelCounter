b6a3cd456e8a2b49e76bff72ec470a8f86be9014
Lucas Simas <simas.lucas@hotmail.com>
2024-10-28 11:51:17 +0000

    Initial Commit

---
+++ PadelCounter Watch App/PadelCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelCounterApp.swift
+//  PadelCounter Watch App
+//
+//  Created by Lucas Simas on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PadelCounter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PadelCounter Watch AppTests/PadelCounter_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PadelCounter_Watch_AppTests.swift
+//  PadelCounter Watch AppTests
+//
+//  Created by Lucas Simas on 28/10/2024.
+//
+
+import Testing
+@testable import PadelCounter_Watch_App
+
+struct PadelCounter_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
